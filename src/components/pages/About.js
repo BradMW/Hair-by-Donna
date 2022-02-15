02d4f13f9b0a9ae30e412d547fee6ef1f0a7c86c
BradMW <bodell94@yahoo.com>
2022-02-15 11:12:28 -0600

    more css changes to about. replaced the picture

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const styles = {
     card: {
-      padding: "10px",
+      paddingLeft: "30px",
     //   justifyContent: "center",
       display: "grid",
       gridTemplateColumns: "2fr, 1fr",
@@ -11,9 +11,13 @@ const styles = {
       backgroundColor: "#0D0D0D"       
     },
     img: {
-        width: "80%",
-        paddingRight: "20px",
-        margin: "10px"
+        maxHeight: "600px",
+        paddingRight: "10px",
+        margin: "10px",
+        borderRadius: '20px 20px 20px 20px',
+        boxShadow: "0px 0px 24px 2px #737373",
+        padding: "10px",
+        backgroundColor: "#BFBFBF"
     },
     title: {
         
@@ -47,7 +51,7 @@ export default function About() {
         </h1>
     </div>
   {/* <img src="../../../me.jpg" className="card-img-top" alt=""/> */}
-  <img style={styles.img} src={require(`../../images/pexels-cottonbro-3992874.jpg`).default}/>
+  <img style={styles.img} src={require(`../../images/salonChair.jpg`).default}/>
             </div>
   <div style={styles.txt} >
     <p className="card-text">Welcome to the world of beauty... Our mission is to have all of our clients look and feel their best everyday. We strongly believe that everyone wants to portray a positive look that is flexible witht the diversity of their lifestyle. Whether its your first interview, or you just want to mix things up, we are here to help you look your best and make the best first impressions everywhere you go.
