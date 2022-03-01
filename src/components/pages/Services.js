2f9ed1d69a56afd01316f45166255208798cd10d
BradMW <bodell94@yahoo.com>
2022-03-01 10:22:59 -0600

    added styling to the services tab

--- src/components/pages/Services.js
+++ src/components/pages/Services.js
@@ -4,14 +4,30 @@ const styles = {
     div: {
       height: "100%",
       width: "100%",
-      marginTop: "120px"
+      marginTop: "90px",
+      justifyContent: "space-evenly",
+      display: "inline-flex",
+      flexWrap: "wrap",
+      backgroundColor: "#0D0D0D"
       
     },
     img: {
-      width: "80%",
-      paddingRight: "20px",
-      margin: "10px"
+      // width: "50%",
+      justifyContent: "start",
+      display: "inline-flex",
+      flexWrap: "wrap",
+      
   },
+    box: {
+      borderRadius: '10px 10px 10px 10px',
+      boxShadow: "0px 0px 24px 2px #737373",
+      padding: "10px",
+      color: "white",
+      backgroundColor: "#BFBFBF",
+      margin: "10px",
+      textAlign: "center"
+      
+    }
     
 }
 
@@ -19,62 +35,69 @@ export default function Services() {
 
     return(
         <div style={styles.div}>
-            <ul className="list-group">
-  <li className="list-group-item d-flex justify-content-between align-items-center">
+          <div style={styles.img}>
+<img style={styles.img} src={require(`../../images/IMG_7820.jpg`).default}/>
+</div>
+            <ul  className="list-group">
+  <li style={styles.box} className="list-group-item d-flex justify-content-between align-items-center">
     Women Haircut
     <span className="badge bg-primary rounded-pill"> $42</span>
   </li>
-  <li className="list-group-item d-flex justify-content-between align-items-center">
+  <li style={styles.box} className="list-group-item d-flex justify-content-between align-items-center">
     Women Haircut & style
     <span className="badge bg-primary rounded-pill"> $42</span>
   </li>
-  <li className="list-group-item d-flex justify-content-between align-items-center">
+  <li style={styles.box} className="list-group-item d-flex justify-content-between align-items-center">
     Men Haircut 
     <span className="badge bg-primary rounded-pill"> $20-$25</span>
   </li>
-  <li className="list-group-item d-flex justify-content-between align-items-center">
+  <li style={styles.box} className="list-group-item d-flex justify-content-between align-items-center">
     Shampoo Blow Dry
     <span className="badge bg-primary rounded-pill"> $25</span>
   </li>
-  <li className="list-group-item d-flex justify-content-between align-items-center">
+  <li style={styles.box} className="list-group-item d-flex justify-content-between align-items-center">
     Color
     <span className="badge bg-primary rounded-pill"> $70 and up</span>
   </li>
-  <li className="list-group-item d-flex justify-content-between align-items-center">
+  <li style={styles.box} className="list-group-item d-flex justify-content-between align-items-center">
     Surface Highlight
     <span className="badge bg-primary rounded-pill"> $70</span>
   </li>
-  <li className="list-group-item d-flex justify-content-between align-items-center">
+  <li style={styles.box} className="list-group-item d-flex justify-content-between align-items-center">
     Partial Highlight
     <span className="badge bg-primary rounded-pill"> 75$</span>
   </li>
-  <li className="list-group-item d-flex justify-content-between align-items-center">
+  <li style={styles.box} className="list-group-item d-flex justify-content-between align-items-center">
     Full Highlight
     <span className="badge bg-primary rounded-pill"> $95</span>
   </li>
-  <li className="list-group-item d-flex justify-content-between align-items-center">
+  <li style={styles.box} className="list-group-item d-flex justify-content-between align-items-center">
     Perm
     <span className="badge bg-primary rounded-pill"> $75+</span>
   </li>
-  <li className="list-group-item d-flex justify-content-between align-items-center">
+  <li style={styles.box} className="list-group-item d-flex justify-content-between align-items-center">
     Roller Set
     <span className="badge bg-primary rounded-pill"> $20</span>
   </li>
-  <li className="list-group-item d-flex justify-content-between align-items-center">
+  <li style={styles.box} className="list-group-item d-flex justify-content-between align-items-center">
     Updo
     <span className="badge bg-primary rounded-pill"> $50</span>
   </li>
-  <li className="list-group-item d-flex justify-content-between align-items-center">
+  <li style={styles.box} className="list-group-item d-flex justify-content-between align-items-center">
     Wax - Lip / Brow / Chin
     <span className="badge bg-primary rounded-pill"> $20</span>
   </li>
-  <li className="list-group-item d-flex justify-content-between align-items-center">
+  <li style={styles.box} className="list-group-item d-flex justify-content-between align-items-center">
     4-8 Foils
     <span className="badge bg-primary rounded-pill"> $50</span>
   </li>
 </ul>
 <div style={styles.img}>
-<img style={styles.img} src={require(`../../images/pexels-cottonbro-3992874.jpg`).default}/>
+<img style={styles.img} src={require(`../../images/IMG_7820.jpg`).default}/>
+</div>
+
+<div style={styles.img}>
+<img style={styles.img} src={require(`../../images/IMG_7820.jpg`).default}/>
 </div>
         </div>    
 ) 
